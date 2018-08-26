9d9376bc3d3c2b33f1a1e4457d6f04f016b9498f
unknown <38478896+nlee728@users.noreply.github.com>
2018-08-25 20:30:43 -0400

    added pages

---
+++ assets/javascript/app.js
@@ -0,0 +1,109 @@
+//id = weather for weather feature
+
+//CLOCK
+function show(time) {
+    $('#clock').append(`<p>${time}</p>`);
+}
+show(moment().format("MM DD, YYYY hh:mm:ss"));
+
+//PROFILE FORM
+ // Capture Button Click
+  $("#add-user").on("click", function(event) {
+    // prevent page from refreshing when form tries to submit itself
+    event.preventDefault();
+
+    // Capture user inputs and store them into variables
+    var name = $("#name-input").val().trim();
+    var email = $("#email-input").val().trim();
+    var age = $("#age-input").val().trim();
+    var comment = $("#comment-input").val().trim();
+
+    // Console log each of the user inputs to confirm we are receiving them
+    console.log(name);
+    console.log(email);
+    console.log(age);
+    console.log(comment);
+
+    // Replaces the content in the "recent-member" div with the new info
+    $("#name-display").text(name);
+    $("#email-display").text(email);
+    $("#age-display").text(age);
+    $("#comment-display").text(comment);
+
+    // Clear localStorage
+    localStorage.clear();
+
+    // Store all content into localStorage
+    localStorage.setItem("name", name);
+    localStorage.setItem("email", email);
+    localStorage.setItem("age", age);
+    localStorage.setItem("comment", comment);
+  });
+
+  // By default display the content from localStorage
+  $("#name-display").text(localStorage.getItem("name"));
+  $("#email-display").text(localStorage.getItem("email"));
+  $("#age-display").text(localStorage.getItem("age"));
+  $("#comment-display").text(localStorage.getItem("comment"));
+
+
+//TO DO LIST
+  var list = JSON.parse(localStorage.getItem("todolist"));
+
+    // Checks to see if the todolist exists in localStorage and is an array currently
+    // If not, set a local list variable to an empty array
+    // Otherwise list is our current list of todos
+    if (!Array.isArray(list)) {
+      list = [];
+    }
+
+    function putOnPage() {
+
+      $("#todo-list").empty(); // empties out the html
+
+      var insideList = JSON.parse(localStorage.getItem("todolist"));
+
+      // Checks to see if we have any todos in localStorage
+      // If we do, set the local insideList variable to our todos
+      // Otherwise set the local insideList variable to an empty array
+      if (!Array.isArray(insideList)) {
+        insideList = [];
+      }
+
+      // render our insideList todos to the page
+      for (var i = 0; i < insideList.length; i++) {
+        var p = $("<p>").text(insideList[i]);
+        var b = $("<button class='delete'>").text("x").attr("data-index", i);
+        p.prepend(b);
+        $("#todo-list").prepend(p);
+      }
+    }
+
+    // render our todos on page load
+    putOnPage();
+
+    $(document).on("click", "button.delete", function() {
+      var todolist = JSON.parse(localStorage.getItem("todolist"));
+      var currentIndex = $(this).attr("data-index");
+
+      // Deletes the item marked for deletion
+      todolist.splice(currentIndex, 1);
+      list = todolist;
+
+      localStorage.setItem("todolist", JSON.stringify(todolist));
+
+      putOnPage();
+    });
+
+    $("input[type='submit']").on("click", function(event) {
+      event.preventDefault();
+      // Setting the input value to a variable and then clearing the input
+      var val = $("input[type='text']").val();
+      $("input[type='text']").val("");
+
+      // Adding our new todo to our local list variable and adding it to local storage
+      list.push(val);
+      localStorage.setItem("todolist", JSON.stringify(list));
+
+      putOnPage();
+    });
