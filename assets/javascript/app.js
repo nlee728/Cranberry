1dcedfd77e0df22e4ec5cf6f634a57226e047043
unknown <38478896+nlee728@users.noreply.github.com>
2018-09-06 18:19:01 -0400

    working clock and weather bar

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,62 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC92DrbxO7k9qQkk7_Pk3W8XFr47XACWcw",
+    authDomain: "cranberry-9c1f7.firebaseapp.com",
+    databaseURL: "https://cranberry-9c1f7.firebaseio.com",
+    projectId: "cranberry-9c1f7",
+    storageBucket: "",
+    messagingSenderId: "580674378498"
+  };
+  firebase.initializeApp(config);
+
+  // Create a variable to reference the database.
+  var database = firebase.database();
+
+  //LOGIN
+  // Initial Login Values
+  var name = "";
+  var email = "";
+  var pw = 0;
+
+  // Capture Login Button Click
+  $("#login-btn").on("click", function(event) {
+    event.preventDefault();
+
+    // Grabbed values from text boxes
+    name = $("#username-input").val().trim();
+    email = $("#email-input").val().trim();
+    pw = $("#pw-input").val().trim();
+
+    // Code for handling the push
+    database.ref("UserList/" + name).push({
+      name: name,
+      email: email,
+      pw: pw,
+      dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+
+  });
+
+  // Firebase watcher .on("child_added"
+  database.ref().on("child_added", function(snapshot) {
+    // storing the snapshot.val() in a variable for convenience
+    var sv = snapshot.val();
+
+    // Console.logging the last user's data
+    console.log(sv.name);
+    console.log(sv.email);
+    console.log(sv.pw);
+
+    // Change the HTML to reflect
+    $("#name-display").text("Hey there, " + sv.name);
+
+    // Handle the errors
+  }, function(errorObject) {
+    console.log("Errors handled: " + errorObject.code);
+  });
+
+
 //Add Tank PopUp Form
-// Get the modal
 var modal = document.getElementById('id01');
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -8,7 +65,7 @@ window.onclick = function(event) {
     }
 }
 
-// Get the modal
+//Login Popup Form
 var modal2 = document.getElementById('id02');
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
@@ -34,70 +91,44 @@ window.onclick = function(event) {
 
    
 //CLOCK
-function show(time) {
-    $("#clock").append(`<p>${time}</p>`);
+function update() {
+  $('#clock').html(moment().format('D MMMM YYYY H:mm:ss a'));
 }
-show(moment().format('MMMM Do YYYY, h:mm:ss a'));
+setInterval(update, 1000);
 
-
-// //PROFILE FORM
-//  // Capture Button Click
-//   $("#add-user").on("click", function(event) {
-//     // prevent page from refreshing when form tries to submit itself
-//     event.preventDefault();
-
-//     // Capture user inputs and store them into variables
-//     var name = $("#name-input").val().trim();
-//     var email = $("#email-input").val().trim();
-//     var age = $("#age-input").val().trim();
-//     var comment = $("#comment-input").val().trim();
-
-//     // Console log each of the user inputs to confirm we are receiving them
-//     console.log(name);
-//     console.log(email);
-//     console.log(age);
-//     console.log(comment);
-
-//     // Replaces the content in the "recent-member" div with the new info
-//     $("#name-display").text(name);
-//     $("#email-display").text(email);
-//     $("#age-display").text(age);
-//     $("#comment-display").text(comment);
-
-//     // Clear localStorage
-//     localStorage.clear();
-
-//     // Store all content into localStorage
-//     localStorage.setItem("name", name);
-//     localStorage.setItem("email", email);
-//     localStorage.setItem("age", age);
-//     localStorage.setItem("comment", comment);
-//   });
-
-//   // By default display the content from localStorage
-//   $("#name-display").text(localStorage.getItem("name"));
-//   $("#email-display").text(localStorage.getItem("email"));
-//   $("#age-display").text(localStorage.getItem("age"));
-//   $("#comment-display").text(localStorage.getItem("comment"));
-
+//PROGRESS BAR
+function move() {
+  var elem = document.getElementById("myBar"); 
+  var width = 10;
+  var id = setInterval(frame, 10);
+  function frame() {
+      if (width >= 100) {
+          clearInterval(id);
+      } else {
+          width++; 
+          elem.style.width = width + '%'; 
+          elem.innerHTML = width * 1 + '%';
+      }
+  }
+}
 
 // //TO DO LIST
-//   var list = JSON.parse(localStorage.getItem("todolist"));
+//   var list = JSON.parse(localStorpw.getItem("todolist"));
 
 //     if (!Array.isArray(list)) {
 //       list = [];
 //     }
 
-//     function putOnPage() {
+//     function putOnPpw() {
 //       $("#todo-list").empty(); // empties out the html
 
-//       var insideList = JSON.parse(localStorage.getItem("todolist"));
+//       var insideList = JSON.parse(localStorpw.getItem("todolist"));
 
 //       if (!Array.isArray(insideList)) {
 //         insideList = [];
 //       }
 
-//       // render our insideList todos to the page
+//       // render our insideList todos to the ppw
 //       for (var i = 0; i < insideList.length; i++) {
 //         var p = $("<p>").text(insideList[i]);
 //         var b = $("<button class='delete'>").text("x").attr("data-index", i);
@@ -106,20 +137,20 @@ show(moment().format('MMMM Do YYYY, h:mm:ss a'));
 //       }
 //     }
 
-//     // render our todos on page load
-//     putOnPage();
+//     // render our todos on ppw load
+//     putOnPpw();
 
 //     $(document).on("click", "button.delete", function() {
-//       var todolist = JSON.parse(localStorage.getItem("todolist"));
+//       var todolist = JSON.parse(localStorpw.getItem("todolist"));
 //       var currentIndex = $(this).attr("data-index");
 
 //       // Deletes the item marked for deletion
 //       todolist.splice(currentIndex, 1);
 //       list = todolist;
 
-//       localStorage.setItem("todolist", JSON.stringify(todolist));
+//       localStorpw.setItem("todolist", JSON.stringify(todolist));
 
-//       putOnPage();
+//       putOnPpw();
 //     });
 
 //     $("input[type='submit']").on("click", function(event) {
@@ -128,10 +159,10 @@ show(moment().format('MMMM Do YYYY, h:mm:ss a'));
 //       var val = $("input[type='text']").val();
 //       $("input[type='text']").val("");
 
-//       // Adding our new todo to our local list variable and adding it to local storage
+//       // Adding our new todo to our local list variable and adding it to local storpw
 //       list.push(val);
-//       localStorage.setItem("todolist", JSON.stringify(list));
+//       localStorpw.setItem("todolist", JSON.stringify(list));
 
-//       putOnPage();
+//       putOnPpw();
 //     });
 
